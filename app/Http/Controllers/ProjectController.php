0601c14da30fd1322e1f706192d000da603d0fd3
Chab <palexandrechabot@gmail.com>
2026-08-27 13:39:11 -0400

    feat : implement controller for cleaner architecture design

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index() {
+        return view("index");
+    }
+//
+
+    public function create() {
+        return view("index");
+    }
+
+    public function modify() {
+        return view("modify");
+    }
+
+    public function delete() {
+        return view("delete");
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [ProjectController::class, 'index']);
 
 
-Route::get("/create",function () {
-    return view("create");
-});
+Route::get("/create",[ProjectController::class, 'create']);
 
-Route::get("/modfiy",function () {
-    return view("modify");
-});
+Route::get("/modfiy",[ProjectController::class, 'modify']);
 
-Route::post("/delete",function () {
-    return view("delete");
-});
+Route::post("/delete",[ProjectController::class,'delete'];
+
+
