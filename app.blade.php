74e93969acfc71bf1f814f70ced82c24801d59c8
Chab <palexandrechabot@gmail.com>
2026-08-27 14:03:11 -0400

    feat : implement base for all page with app.blade.php

---
+++ app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML/>
+<html>
+    <head>
+        <title>{{ "LOG KEEPER".$title || "LOG KEEPER"}}<title/>
+    <head/>
+
+    @include('components.navbar')
+
+    <body>
+        @yield('content')
+    <body/>
+
+
+    @include('components.footer')
+
+<html/>
+
