d6e09a2285155392002aba695d363895ccf6072e
Chab <palexandrechabot@gmail.com>
2026-09-03 23:59:41 -0400

    feat : implement error handling on forms

--- resources/views/components/modify/counted-textarea-field.blade.php
+++ resources/views/components/modify/counted-textarea-field.blade.php
@@ -6,18 +6,26 @@
     'value' => '',
     'placeholder' => '',
     'large' => false,
+    'required' => false,
 ])
 
 <div class="edit-form__label-row">
-    <label class="edit-form__label" for="{{ $id }}">{{ $label }}</label>
+    <label class="edit-form__label" for="{{ $id }}">
+        {{ $label }}@if ($required)<span class="edit-form__required" aria-hidden="true">*</span>@endif
+    </label>
     <span class="edit-form__counter" data-counter-for="{{ $id }}" data-max="{{ $maxlength }}">{{ mb_strlen($value) }} / {{ $maxlength }}</span>
 </div>
 <textarea
     id="{{ $id }}"
     name="{{ $name }}"
-    class="edit-form__textarea @if($large) edit-form__textarea--large @endif"
+    @class([
+        'edit-form__textarea',
+        'edit-form__textarea--large' => $large,
+        'edit-form__textarea--error' => $errors->has($name),
+    ])
     maxlength="{{ $maxlength }}"
     rows="{{ $large ? 10 : 3 }}"
     data-autogrow
     placeholder="{{ $placeholder }}"
+    @required($required)
 >{{ $value }}</textarea>
--- resources/views/components/modify/title-field.blade.php
+++ resources/views/components/modify/title-field.blade.php
@@ -15,7 +15,11 @@
     id="log-title"
     name="title"
     type="text"
-    class="edit-form__input edit-form__input--title"
+    @class([
+        'edit-form__input',
+        'edit-form__input--title',
+        'edit-form__input--error' => $errors->has('title'),
+    ])
     value="{{ $value }}"
     @if($maxlength) maxlength="{{ $maxlength }}" @endif
     placeholder="Enter log title..."
