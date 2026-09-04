1613767e20a45f8a0737726f49bc6d27397d16c3
Chab <palexandrechabot@gmail.com>
2026-09-03 21:50:22 -0400

    feat : implement project creation page view

---
+++ resources/views/createProject.blade.php
@@ -0,0 +1,48 @@
+@extends('layout.app')
+
+@section('content')
+
+    <form method="POST" action="{{ route('project.store') }}" class="edit-form">
+        @csrf
+
+        @if ($errors->any())
+            <div class="edit-form__errors">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="edit-form__actions">
+            <a href="{{ url('/') }}" class="edit-form__button edit-form__button--cancel">
+                Cancel
+            </a>
+            <button type="submit" class="edit-form__button edit-form__button--save">
+                Save new project
+            </button>
+        </div>
+
+        <div class="edit-form__field">
+            <x-modify.title-field :maxlength="50" :value="old('title', '')" />
+        </div>
+
+        <div class="edit-form__field">
+            <label class="edit-form__label">Tags</label>
+            <x-tag-input :tags="old('tags', [])" />
+        </div>
+
+        <div class="edit-form__field">
+            <x-modify.counted-textarea-field
+                id="project-subtitle"
+                name="subtitle"
+                label="Subtitle"
+                :maxlength="50"
+                :value="old('subtitle', '')"
+                placeholder="Write a short subtitle..."
+            />
+        </div>
+    </form>
+
+@endsection
