fd8082e23372bf81e5a3892eedc327dff99fff6f
Chab <palexandrechabot@gmail.com>
2026-09-03 21:46:52 -0400

    feat : implement routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,23 @@ use App\Http\Controllers\ProjectController;
 
 Route::get('/', [ProjectController::class, 'index']);
 
+Route::get('/projects/create',[ProjectController::class, 'createProject'])->name('project.create');
+
+Route::post('/projects',[ProjectController::class, 'storeProject'])->name('project.store');
+
+Route::get('/projects/{id}', [ProjectController::class, 'show'])->name('projects.show');
+
+Route::get('/logs/create', [ProjectController::class, 'createLog'])->name('logs.create');
+
+Route::get('/logs/{id}', [ProjectController::class, 'showLog'])->name('logs.show');
+
+Route::get('/logs/{id}/edit', [ProjectController::class, 'editLog'])->name('logs.edit');
+
+Route::post('/logs/{id}/update', [ProjectController::class, 'updateLog'])->name('logs.update');
+
+Route::post('/logs/{id}/delete', [ProjectController::class, 'deleteLog'])->name('logs.delete');
+
+
 
 Route::get("/create",[ProjectController::class, 'create']);
 
