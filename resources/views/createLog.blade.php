44dba18b5a57cf7d64a1ec140ca2895b303f361c
Chab <palexandrechabot@gmail.com>
2026-09-03 22:58:14 -0400

    feat : implement correct route to save project
    Also remove field for date and project selection

--- resources/views/createLog.blade.php
+++ resources/views/createLog.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <form method="POST" action="{{ route('logs.store') }}" class="edit-form">
+    <form method="POST" action="{{ route('logs.store', $project) }}" class="edit-form">
         @csrf
 
         @if ($errors->any())
@@ -16,7 +16,7 @@
         @endif
 
         <div class="edit-form__actions">
-            <a href="{{ url('/') }}" class="edit-form__button edit-form__button--cancel">
+            <a href="{{ route('projects.show', $project) }}" class="edit-form__button edit-form__button--cancel">
                 Cancel
             </a>
             <button type="submit" class="edit-form__button edit-form__button--save">
@@ -26,26 +26,6 @@
 
         <div class="edit-form__field">
             <x-modify.title-field :value="old('title', '')" />
-        </div>
-
-        <div class="edit-form__field">
-            <x-modify.select-field
-                id="log-project"
-                name="project_id"
-                label="Project"
-                :options="$projects->pluck('name', 'id')"
-                :selected="old('project_id')"
-                placeholder="Select a project"
-            />
-        </div>
-
-        <div class="edit-form__field">
-            <x-modify.date-field
-                id="log-date"
-                name="date"
-                label="Date"
-                :value="old('date', now()->format('Y-m-d'))"
-            />
         </div>
 
         <div class="edit-form__field">
