a86a0bbdf2ec250c8b28416ad6b41e89a022c2e4
Chab <palexandrechabot@gmail.com>
2026-09-03 21:54:08 -0400

    feat : implement log title field component

---
+++ resources/views/components/modify/title-field.blade.php
@@ -0,0 +1,22 @@
+@props([
+    'value' => '',
+    'maxlength' => null,
+])
+
+@if($maxlength)
+    <div class="edit-form__label-row">
+        <label class="edit-form__label" for="log-title">Title</label>
+        <span class="edit-form__counter" data-counter-for="log-title" data-max="{{ $maxlength }}">{{ mb_strlen($value) }} / {{ $maxlength }}</span>
+    </div>
+@else
+    <label class="edit-form__label" for="log-title">Title</label>
+@endif
+<input
+    id="log-title"
+    name="title"
+    type="text"
+    class="edit-form__input edit-form__input--title"
+    value="{{ $value }}"
+    @if($maxlength) maxlength="{{ $maxlength }}" @endif
+    placeholder="Enter log title..."
+>
