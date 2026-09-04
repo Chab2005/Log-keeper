003e5e5cb4d3cdb3ef3808c370b937820753efc0
Chab <palexandrechabot@gmail.com>
2026-09-03 21:41:32 -0400

    feat : implement component for header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<header class="site-header">
+    <div class="site-header__inner">
+        <span class="site-header__title">
+            <a href="/">Log Keeper</a>
+        </span>
+    </div>
+</header>
