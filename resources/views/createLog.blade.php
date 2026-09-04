a76b096801b3082070f2f90dc4706af0340f2b2b
Chab <palexandrechabot@gmail.com>
2026-09-03 22:30:14 -0400

    feat : implement correct route for new entries

--- resources/views/createLog.blade.php
+++ resources/views/createLog.blade.php
@@ -2,8 +2,18 @@
 
 @section('content')
 
-    <form method="POST" action="{{ route('logs.create') }}" class="edit-form">
+    <form method="POST" action="{{ route('logs.store') }}" class="edit-form">
         @csrf
+
+        @if ($errors->any())
+            <div class="edit-form__errors">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
         <div class="edit-form__actions">
             <a href="{{ url('/') }}" class="edit-form__button edit-form__button--cancel">
@@ -15,12 +25,32 @@
         </div>
 
         <div class="edit-form__field">
-            <x-modify.title-field />
+            <x-modify.title-field :value="old('title', '')" />
+        </div>
+
+        <div class="edit-form__field">
+            <x-modify.select-field
+                id="log-project"
+                name="project_id"
+                label="Project"
+                :options="$projects->pluck('name', 'id')"
+                :selected="old('project_id')"
+                placeholder="Select a project"
+            />
+        </div>
+
+        <div class="edit-form__field">
+            <x-modify.date-field
+                id="log-date"
+                name="date"
+                label="Date"
+                :value="old('date', now()->format('Y-m-d'))"
+            />
         </div>
 
         <div class="edit-form__field">
             <label class="edit-form__label">Tags</label>
-            <x-tag-input />
+            <x-tag-input :tags="old('tags', [])" />
         </div>
 
         <div class="edit-form__field">
@@ -29,6 +59,7 @@
                 name="summary"
                 label="Summary"
                 :maxlength="255"
+                :value="old('summary', '')"
                 placeholder="Write a short summary..."
             />
         </div>
@@ -39,6 +70,7 @@
                 name="description"
                 label="Description"
                 :maxlength="1024"
+                :value="old('description', '')"
                 placeholder="Write a detailed description..."
                 :large="true"
             />
