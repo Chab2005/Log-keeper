2da34c2dfe2ce797a6e90fb8c624e9c8562a4933
Chab <palexandrechabot@gmail.com>
2026-09-03 21:47:17 -0400

    feat : implement index page view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
 
-<h1>THIS IS A TEST</h1>
+    <section class="index-hero">
+        <h1 class="index-hero__title">All Logs</h1>
+    </section>
+
+    <section class="index-toolbar">
+        <x-search-bar
+            placeholder="Query projects..."
+            :url="route('project.create')"
+            label="New project"
+        />
+    </section>
+
+    <section class="index-grid">
+        @foreach ($projects as $project)
+            <x-project-card
+                :id="$project['id']"
+                :title="$project['title']"
+                :tags="$project['tags']"
+                :last-modified="$project['lastModified']"
+            />
+        @endforeach
+    </section>
 
 @endsection
