4ae62a4e124435029eb10e80e1e9467605e0188f
Chab <palexandrechabot@gmail.com>
2026-09-03 21:33:15 -0400

    feat : implement Entry model

---
+++ app/Models/Entry.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Entry extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'date',
+        'title',
+        'summary',
+        'description',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'date' => 'date',
+        ];
+    }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
