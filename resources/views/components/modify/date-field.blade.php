c48c8fac447395d9f0d1dcfecbe1dc88132a96a1
Chab <palexandrechabot@gmail.com>
2026-09-03 23:01:25 -0400

    refactor : removed unused component view

--- resources/views/components/modify/date-field.blade.php
+++
@@ -1,9 +0,0 @@
-@props([
-    'id',
-    'name',
-    'label',
-    'value' => '',
-])
-
-<label class="edit-form__label" for="{{ $id }}">{{ $label }}</label>
-<input id="{{ $id }}" name="{{ $name }}" type="date" class="edit-form__input" value="{{ $value }}">
