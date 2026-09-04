0baf3fb3b9e972eac0383e7a71c8969a7789882a
Chab <palexandrechabot@gmail.com>
2026-09-03 21:38:48 -0400

    feat : implement app layout view

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,20 +1,24 @@
-<!DOCTYPE HTML/>
-<html>
+<!DOCTYPE html>
+<html lang="en">
     @php
-        $title = "";
+        $title = $title ?? '';
     @endphp
     <head>
-        <title>LOG KEEPER @if(!empty($title))- {{ $title }}@endif</title>
-    <head/>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>LOG KEEPER @if(!empty($title)) - {{ $title }} @endif</title>
+        @vite(['resources/css/app.css', 'resources/js/app.ts'])
+    </head>
 
-    @include('components.header')
+    <body class="flex min-h-screen flex-col bg-background font-sans text-on-background">
+        <x-header />
 
-    <body>
-        @yield('content')
+        <main class="flex-1 pt-16">
+            <div class="page-content">
+                @yield('content')
+            </div>
+        </main>
+
+        <x-footer />
     </body>
-
-
-    @include('components.footer')
-
 </html>
-
