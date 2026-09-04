39d352a94afb0c9bee86ee4e1f54536775d733af
Chab <palexandrechabot@gmail.com>
2026-09-03 21:42:32 -0400

    feat : implement log entry component on a project page

---
+++ resources/views/components/log-entry.blade.php
@@ -0,0 +1,28 @@
+@props([
+    'id',
+    'title',
+    'timestamp' => null,
+    'description' => null,
+    'tags' => [],
+])
+
+<a href="{{ route('logs.show', $id) }}" class="log-entry">
+    <div class="log-entry__header">
+        <h3 class="log-entry__title">{{ $title }}</h3>
+        @if ($timestamp)
+            <span class="log-entry__timestamp">{{ $timestamp }}</span>
+        @endif
+    </div>
+
+    @if ($description)
+        <p class="log-entry__description">{{ $description }}</p>
+    @endif
+
+    @if (!empty($tags))
+        <div class="log-entry__tags">
+            @foreach ($tags as $tag)
+                <span class="log-entry__tag">#{{ $tag }}</span>
+            @endforeach
+        </div>
+    @endif
+</a>
