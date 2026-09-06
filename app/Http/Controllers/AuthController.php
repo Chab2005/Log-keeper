38bf0a88e4c00f15fe7af9c3fd7f02a2b6939e07
Chab <palexandrechabot@gmail.com>
2026-09-06 16:38:00 -0400

    feat : implement log out

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,4 +65,15 @@ class AuthController extends Controller
 
         return redirect()->intended('/');
     }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
 }
